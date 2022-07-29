feb6898ad7747c2795112af762a799efdee04208
MohammadRhamdan <moh.rhamdanf52121023@gmail.com>
2022-07-29 20:28:01 +0800

    first commit

---
+++ assets/style/style.js
@@ -0,0 +1,59 @@
+// MENU HAMBUGER
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+  nav.classList.toggle('slide')
+});
+
+// slide show
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); 
+}
+
+// BACKGROUND BERUBAH PADA HOME
+const [red, green, blue] = [69, 111, 225]
+const section1 = document.querySelector('.flex-row')
+
+window.addEventListener('scroll', () => {
+  let y = 1 + (window.scrollY || window.pageYOffset) / 150
+  y = y < 1 ? 1 : y
+  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+  section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+})
+
+
+// DROP DOWN
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
